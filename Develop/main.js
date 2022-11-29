223a2dbd6e8dce5995f6305d96595aba13199b05
rjschroeder <are.jay.schroeder@gmail.com>
2022-11-29 17:26:23 -0600

    refactored local storage object
    was using an array of objects, realized it was better to use one object and keys instead. Can use keys to match element id's

--- Develop/main.js
+++ Develop/main.js
@@ -2,23 +2,25 @@ let dateElement = $("#currentDay");
 
 $(document).ready(function () {
     $(dateElement).text(moment().format("dddd, MMMM Do"));
-    let loadedData = localStorage.getItem("textData");
-    if(!loadedData){
-        let scheduleModel = [
-            {time: "9am", text: ""},
-            {time: "10am", text: ""},
-            {time: "11am", text: ""},
-            {time: "12pm", text: ""},
-            {time: "1pm", text: ""},
-            {time: "2pm", text: ""},
-            {time: "3pm", text: ""},
-            {time: "4pm", text: ""},
-            {time: "5pm", text: ""},
-        ]
-        localStorage.setItem("textData", scheduleModel);
+    let loadedData = JSON.parse(window.localStorage.getItem("textDataWDS"));
+    if(loadedData === null){
+        let scheduleModel = {
+            "9am": "",
+            "10am": "",
+            "11am": "",
+            "12pm": "",
+            "1pm": "",
+            "2pm": "",
+            "3pm": "",
+            "4pm": "",
+            "5pm": ""
+        }
+        window.localStorage.setItem("textDataWDS", JSON.stringify(scheduleModel));
     }
+
     $(".timeBlock").each(function () {
-        let thisTime = $(this).attr('id');
-        $(this).val(localStorage.getItem(thisTime))
-    })
+        let thisTime = $(this).attr("id");
+        $(`#${thisTime} .blockTextArea`).val(loadedData[thisTime]);
+    });
+
 });
