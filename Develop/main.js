87c56e85a44cf22b1a724d364f32eedc895d233a
rjschroeder <are.jay.schroeder@gmail.com>
2022-11-29 16:51:09 -0600

    loads all data from local storage based on element ids

--- Develop/main.js
+++ Develop/main.js
@@ -1,5 +1,24 @@
-let dateElement = $('#currentDay');
+let dateElement = $("#currentDay");
 
 $(document).ready(function () {
-    $(dateElement).text(moment().format("dddd, MMMM Do"))
+    $(dateElement).text(moment().format("dddd, MMMM Do"));
+    let loadedData = localStorage.getItem("textData");
+    if(!loadedData){
+        let scheduleModel = [
+            {time: "9am", text: ""},
+            {time: "10am", text: ""},
+            {time: "11am", text: ""},
+            {time: "12pm", text: ""},
+            {time: "1pm", text: ""},
+            {time: "2pm", text: ""},
+            {time: "3pm", text: ""},
+            {time: "4pm", text: ""},
+            {time: "5pm", text: ""},
+        ]
+        localStorage.setItem("textData", scheduleModel);
+    }
+    $(".timeBlock").each(function () {
+        let thisTime = $(this).attr('id');
+        $(this).val(localStorage.getItem(thisTime))
+    })
 });
