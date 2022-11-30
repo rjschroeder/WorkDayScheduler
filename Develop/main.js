2345ad875a644fff912ea3d3e31a6cbdee5edbac
rjschroeder <are.jay.schroeder@gmail.com>
2022-11-29 18:33:40 -0600

    added comments to code
    also fixed issue where there was an error on first load

--- Develop/main.js
+++ Develop/main.js
@@ -1,10 +1,17 @@
+//declare element that will contain the current day at the top of the page
 let dateElement = $("#currentDay");
 
+//jQuery function to run as soon as page loads
 $(document).ready(function () {
+    //sets the earlier declared element to display current date
     $(dateElement).text(moment().format("dddd, MMMM Do"));
+
+    //try to load the saved data into a variable
     let loadedData = JSON.parse(window.localStorage.getItem("textDataWDS"));
+
+    //if there is nothing saved, initiate a model of what the data will be saved in
     if(loadedData === null){
-        let scheduleModel = {
+        loadedData = {
             "9am": "",
             "10am": "",
             "11am": "",
@@ -15,32 +22,48 @@ $(document).ready(function () {
             "4pm": "",
             "5pm": ""
         }
-        window.localStorage.setItem("textDataWDS", JSON.stringify(scheduleModel));
+        //make sure to save at least one instance of this model
+        window.localStorage.setItem("textDataWDS", JSON.stringify(loadedData));
     }
 
+    //load the saved data into the time blocks
     $(".timeBlock").each(function () {
+        //the key for the saved data will be equal to the element id
         let thisTime = $(this).attr("id");
+
+        //sets the text of the text area child as the value from localstorage using the matching key
         $(`#${thisTime} .blockTextArea`).val(loadedData[thisTime]);
     });
 
+    //saves the entered text when save button is clicked
     $(".blockSaveButton").on("click", function () {
         let enteredText = $(this).siblings(".blockTextArea").val();
         let thisTime = $(this).parent().attr("id");
+
+        //again, uses keys and matching id's to save the correct text in the correct spot
         loadedData[thisTime] = enteredText;
         window.localStorage.setItem("textDataWDS", JSON.stringify(loadedData));
     })
 
+    //interval function to update the time blocks every 10 seconds
     let interval = setInterval(() => {
+        //gets current hour (0-23) from moment.js
+        //this isnt good becuase the object reads in 12-hour time
         let currentHour = moment().hours();
         $(".timeBlock").each(function () {
             let thisTime = $(this).attr("id");
+            //if the block we are checking is a 1 digit am
             if(thisTime.length === 3 && thisTime[1] === "a") {
                 thisTime = parseInt(thisTime[0]);
+            //if the block we are checking is a 2 digit am(however, includes noon)
             } else if(thisTime.length === 4) {
                 thisTime = parseInt(thisTime[0] + thisTime[1]);
+            //if the block we are checking is a 1 digit pm
+            //if so, adds 12 hours to match the hour of the day
             } else if (thisTime.length === 3 && thisTime[1] === "p") {
                 thisTime = parseInt(thisTime[0]) + 12;
             }
+            //assign classes based on wether or not current hour has passed each block
             if(thisTime < currentHour){
                 $(this).addClass("past");
             } else if (thisTime === currentHour) {
@@ -52,6 +75,5 @@ $(document).ready(function () {
                 $(this).addClass("future");
             }
         })
-    }, 1000)
-
+    }, 10000)
 });
